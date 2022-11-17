6bfd0152de3df24898fc7a79bac864a133ce0b46
= <bernhard.birnbaum@st.ovgu.de>
2022-11-17 17:43:30 +0100

    updated some stuff

---
+++ SMKITS5/utility/dockerImportDefaults.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker_scr=./stego-docker.sh
+
+if [ ! -f $docker_scr ]; then
+    echo "Error: '$docker_scr' not found!"
+    exit 2
+fi
+
+$docker_scr --import ./stego-attrib.sh
+$docker_scr --import ./coverData
+
+exit
