6932855316f47afeef052f6e4d9f16a1b10cfd8c
= <bernhard.birnbaum@st.ovgu.de>
2022-11-30 16:46:55 +0100

    removed jphide stuff

--- SMKITS5/utility/dockerImportDefaults.sh
+++ SMKITS5/utility/dockerImportDefaults.sh
@@ -10,12 +10,13 @@ fi
 $docker_scr --import ./stego-attrib.sh
 $docker_scr --import ./coverData
 
-cp ./utility/jpfix/jphide-auto ./jphide-auto
-cp ./utility/jpfix/jpseek-auto ./jpseek-auto
-$docker_scr --import ./jphide-auto
-$docker_scr --import ./jpseek-auto
-rm -f ./jphide-auto
-rm -f ./jpseek-auto
+#JPHIDE: not implemented, its broken!
+#cp ./utility/jpfix/jphide-auto ./jphide-auto
+#cp ./utility/jpfix/jpseek-auto ./jpseek-auto
+#$docker_scr --import ./jphide-auto
+#$docker_scr --import ./jpseek-auto
+#rm -f ./jphide-auto
+#rm -f ./jpseek-auto
 
 cp ./utility/stegbreakfix/stegbreak-fix ./stegbreak-fix
 cp ./utility/stegbreakfix/rules.ini ./stegbreak-rules.ini
