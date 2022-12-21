44a4ce9854790df72d32f22bfea73c02b08ef6af
= <bernhard.birnbaum@st.ovgu.de>
2022-12-21 12:19:49 +0100

    update comments

--- SMKITS5/utility/combineCoverData.sh
+++ SMKITS5/utility/combineCoverData.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+#set output directory name
 outdir=./coverData
+
+#set final testset size
 maxsize=1024
+
+#set amount of bows2-images
 blackwhite=192
 
+#use all images from there
 loc_private=./private
+#use fixed amount of images from there
 loc_bows2=./bows2
+#use images from there to fill up to given testset size
 loc_alaska=./kaggle-alaska2
 
+#check if data sources are available
 if [ ! -d $loc_private ]; then
 	echo "Error: '$loc_private' not found!"
 	exit 2
@@ -21,27 +30,34 @@ if [ ! -d $loc_alaska ]; then
 	exit 2
 fi
 
+#overwrite output directory
 if [ -d $outdir ]; then
 	rm -dr $outdir
 fi
 mkdir $outdir
 
+#loop and copy all private files
 find $loc_private -maxdepth 1 -type f -name *.jpg | while read COVER; do
 	cp $COVER $outdir/$(basename $COVER)
 	echo "copied $(basename $COVER)"
 done
 
+#loop and copy random bows2-images
 find $loc_bows2 -maxdepth 1 -type f -name *.jpg | sort -R | tail -$blackwhite | while read COVER; do
 	cp $COVER $outdir/$(basename $COVER)
 	echo "copied $(basename $COVER)"
 done
 
+#count images currently in testset
 jpgsdone=$(find $outdir -maxdepth 1 -type f -name *.jpg | wc -l)
+
+#calculate missing sample count
 alaska=$((maxsize-jpgsdone))
 
+#fill up testset to maximum size with alaska images
 find $loc_alaska -maxdepth 1 -type f -name *.jpg | sort -R | tail -$alaska | while read COVER; do
 	cp $COVER $outdir/$(basename $COVER)
 	echo "copied $(basename $COVER)"
 done
 
-exit
+exit 0
