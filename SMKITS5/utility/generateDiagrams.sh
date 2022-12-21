1cced4f18b6d73c0eaf0c5c7d6a24dd987947e4b
= <bernhard.birnbaum@st.ovgu.de>
2022-12-21 13:07:41 +0100

    diagrams script start

---
+++ SMKITS5/utility/generateDiagrams.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#specify input directory (containing csv files!)
+INPUT_DIR=./out-stego-attrib
+if [ $# -ne 0 ]; then
+    INPUT_DIR=${1}
+fi
+
+INPUT_DIR=$(realpath $INPUT_DIR)
+
+#check if directory exists
+if [ ! -d $INPUT_DIR ]; then
+    echo "'$INPUT_DIR' could not be found!"
+    exit 2
+fi
+
+#loop all csvs
+find $INPUT_DIR -maxdepth 1 -type f -name "_*.csv"
+
+exit 0
