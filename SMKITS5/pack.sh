4be0a9b295bed6d3ecd643d02928090c6fb6ac97
Bernhard Birnbaum <73957626+birne420@users.noreply.github.com>
2022-12-27 13:50:48 +0100

    Update pack.sh

--- SMKITS5/pack.sh
+++ SMKITS5/pack.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 archiveName=./stego-release.tar.gz
-archiveInclude="coverData/ dev-artifacts/ docs/ embeddingData/ jphide-src/ steg-src/ common.sh README.md stego-attrib.sh stego-docker.sh stego-docker-importDefaults.sh stego-utils-buildTestset.sh stego-utils-generateDiagrams.sh stego-utils-recompressAndDiffCC.sh stego-utils-testAttrib.sh"
+archiveInclude="coverData/ dev-artifacts/ docs/ embeddingData/ dev-artifacts/ common.sh README.md stego-attrib.sh stego-docker.sh stego-docker-importDefaults.sh stego-utils-buildTestset.sh stego-utils-generateDiagrams.sh stego-utils-recompressAndDiffCC.sh stego-utils-testAttrib.sh"
 
 if [ -f $archiveName ]; then
     rm -f $archiveName
