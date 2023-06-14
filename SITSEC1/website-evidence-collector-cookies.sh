740ca23d840ecba90488ec1f4a556d4668b3a2a3
Bernhard Birnbaum <73957626+birnbaum01@users.noreply.github.com>
2023-06-14 18:37:20 +0200

    Update website-evidence-collector-cookies.sh

--- SITSEC1/website-evidence-collector-cookies.sh
+++ SITSEC1/website-evidence-collector-cookies.sh
@@ -4,28 +4,25 @@ clear
 echo
 echo Willkommen zum Aufruf des Website-Evidence-Collector
 echo
-echo 'Wichtig:' Das Programm erwartet die Anwesenheit der zu testenden App 
-echo direkt auf dem Schreibtisch!
-echo
 echo Es wird die vollstaendige URL der zu untersuchenden Webseite 
 echo benoetigt, d.h. mit 'http(s)' beginnend, z.B. http://www.meineseite.de
 echo
-echo Insofern benoetigt, koennen nachfolgend Cookies angegeben werden, die beim
-echo Aufruf der Website vom WEC mit übertragen werden sollen.
-echo So koennen beispielsweise Cookie-Banner automatisch akzeptiert werden,
-echo um die Untersuchung der Website in diese Richtung auszuweiten.
-echo Wenn nicht benoetigt, Feld leer lassen und mit Enter bestaetigen.
-echo
 echo Weiterhin wird der Verzeichnisname auf dem Schreibtisch fuer 
-echo die Ergebnisse  benoetigt. 
+echo die Ergebnisse benoetigt. 
 echo Verwenden Sie hierfuer bitte keine Sonderzeichen und Umlaute.
 echo Achtung, dieses Verzeichnis wird automatisch auf dem Schreibtisch erzeugt  
 echo und beim Neustart der Anwendung bei Namensgeleichheit überschrieben"!"
 echo  
+echo Insofern benoetigt, koennen nachfolgend Cookies angegeben werden, die beim
+echo Aufruf der Website vom EDPS mit übertragen werden sollen.
+echo So koennen beispielsweise Cookie-Banner automatisch akzeptiert werden,
+echo um die Untersuchung der Website in diese Richtung auszuweiten.
+echo Wenn nicht benoetigt, Feld leer lassen und mit Enter bestaetigen.
+echo
 read -p 'Wurden alle Daten gesichert? (Ja/Nein): ' agreed
 [ $agreed != 'Ja' ] && exit 1
 echo 
-echo Beide Angaben werden nachfolgend abgefragt
+echo Alle drei Angaben werden nachfolgend abgefragt
 echo
 read -p 'vollstaendige URL: ' userurlvar
 read -p 'Cookie-Data: ' cookiedata
@@ -51,9 +48,9 @@ rm -r /media/tester/Datenaustausch/Website-Evidence-Collector-$folder
 echo
 echo
 echo
-echo "Die Untersuchungsergebnisse wurden in das Verzeichnis:" 
+echo "Die Untersuchungsergebnisse wurden in das Verzeichnis" 
 echo $resultpath 
-echo "gespeichert und nach:" 
+echo "gespeichert und nach" 
 echo "/media/tester/Datenaustausch/"Website-Evidence-Collector-$folder.zip
 echo archiviert.
 sleep 10
